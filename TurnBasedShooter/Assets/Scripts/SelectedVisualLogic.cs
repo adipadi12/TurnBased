16a03f2f3052d71c5af564c147e9fbde7408a1d7
adipadi12 <ajha4436@gmail.com>
2024-12-14 09:33:22 +0530

    Learning Events

---
+++ TurnBasedShooter/Assets/Scripts/SelectedVisualLogic.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedVisualLogic : MonoBehaviour
+{
+    [SerializeField]private UnitMovement unit;
+}
